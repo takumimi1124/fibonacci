edb1234a51eb4c0811c8fe8e18e252de8041e4fc
Takumi Masuda <takufrom1124@gmail.com>
2019-08-16 15:31:39 +0900

    指数オーダーのfib関数の実装

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
 'use strict';
+function fb(n) {
+    if (n === 0) {
+        return 0;
+    } else if (n === 1) {
+        return 1;
+    }
+    return fb(n - 1) + fb(n - 2);
+}
+
+const length = 40;
+for (let i = 0; i <= length; i++) {
+    console.log(fb(i));
+}
